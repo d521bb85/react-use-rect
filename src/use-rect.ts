71e4dfd0e5b3d6359b909a073239e6ae2b3d63fc
d521bb85 <d521bb85@gmail.com>
2021-11-24 23:10:34 +0300

    Rewrite useRect hook implementation

---
+++ src/use-rect.ts
@@ -0,0 +1,97 @@
+import { useRef, useEffect, useCallback, useLayoutEffect } from 'react';
+
+export type Rect = { [K in typeof RECT_KEYS[number]]: number };
+
+export type DispatchChange = (rect: Rect) => void;
+
+export interface Options {
+  resize?: boolean;
+}
+
+export type Result = [SetElement, Revalidate];
+
+export type SetElement = (element: Element | null) => void;
+
+export type Revalidate = () => void;
+
+export function useRect(
+  dispatchChange: DispatchChange,
+  options: Options = {}
+): Result {
+  const dispatchChangeRef = useRef(dispatchChange);
+  useEffect(() => {
+    dispatchChangeRef.current = dispatchChange;
+  });
+
+  const optionsRef = useRef(options);
+  useEffect(() => {
+    optionsRef.current = options;
+  });
+
+  const elementRef = useRef<Element | null>(null);
+  const rectRef = useRef<Rect | null>(null);
+
+  const revalidate = useCallback(() => {
+    if (!elementRef.current) {
+      return;
+    }
+
+    const nextRect = elementRef.current.getBoundingClientRect();
+
+    if (shouldDispatchRectChange(rectRef.current, nextRect)) {
+      rectRef.current = nextRect;
+      dispatchChangeRef.current(nextRect);
+    }
+  }, []);
+
+  const resizeObserverRef = useRef<ResizeObserver | null>(null);
+  const setupResizeObserver = useCallback(() => {
+    if (resizeObserverRef.current) {
+      resizeObserverRef.current.disconnect();
+    }
+
+    if (elementRef.current && optionsRef.current.resize) {
+      resizeObserverRef.current = new ResizeObserver(revalidate);
+      resizeObserverRef.current.observe(elementRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (resizeObserverRef.current) {
+        resizeObserverRef.current.disconnect();
+      }
+    };
+  });
+
+  const setElement = useCallback((element: Element | null) => {
+    elementRef.current = element;
+    revalidate();
+    setupResizeObserver();
+  }, []);
+
+  useIsomorphicLayoutEffect(revalidate);
+
+  return [setElement, revalidate];
+}
+
+const RECT_KEYS = [
+  'bottom',
+  'height',
+  'left',
+  'right',
+  'top',
+  'width',
+  'x',
+  'y'
+] as const;
+
+function shouldDispatchRectChange(rect: Rect | null, nextRect: Rect) {
+  return (
+    !rect ||
+    (rect !== nextRect && RECT_KEYS.some((key) => rect[key] !== nextRect[key]))
+  );
+}
+
+const useIsomorphicLayoutEffect =
+  typeof window === 'undefined' ? useEffect : useLayoutEffect;
